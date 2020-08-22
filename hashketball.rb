a36f705ec7308988abd0c4b3317f022f77062cec
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 19:40:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,17 @@ def game_hash
   final_hash = {}
   #home team
   final_hash[:home] = {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => []}
-  
-  
+  #home players
+  final_hash[:home][:players].push(:player_name => "Alan Anderson",
+  :number => 0,
+  :shoe => 16,
+  :points => 22,
+  :rebounds => 12,
+  :assists => 12,
+  :steals => 3,
+  :blocks => 1,
+  :slam_dunks => 1)
+  puts final_hash
   
   
   
