cdfd06c69f729c165ebae8a08ed35e7358809830
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 19:36:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,4 +2,15 @@
 
 def game_hash
   final_hash = {}
+  #home team
+  final_hash[:home] = {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => []}
+  
+  
+  
+  
+  
+  #away team
+  final_hash[:away] = {:team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"], :players => []}
+  
+  final_hash
 end
