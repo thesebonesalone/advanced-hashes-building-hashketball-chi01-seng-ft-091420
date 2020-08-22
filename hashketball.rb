5344f0c2727f79db793b13067f1a7caa8e7e5d06
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 19:46:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -70,5 +70,46 @@ def game_hash
   :steals => 2,
   :blocks => 7,
   :slam_dunks => 2)
+  
+  final_hash[:away][:players].push(:player_name => "Bismack Biyombo",
+  :number => 0,
+  :shoe => 16,
+  :points => 12,
+  :rebounds => 4,
+  :assists => 7,
+  :steals => 22,
+  :blocks => 15,
+  :slam_dunks => 10)
+  
+  final_hash[:away][:players].push(:player_name => "DeSagna Diop",
+  :number => 2,
+  :shoe => 14,
+  :points => 24,
+  :rebounds => 12,
+  :assists => 12,
+  :steals => 4,
+  :blocks => 5,
+  :slam_dunks => 5)
+  
+  final_hash[:away][:players].push(:player_name => "Ben Gordon",
+  :number => 8,
+  :shoe => 15,
+  :points => 33,
+  :rebounds => 3,
+  :assists => 2,
+  :steals => 1,
+  :blocks => 1,
+  :slam_dunks => 0)
+  
+  
+  final_hash[:away][:players].push(:player_name => "Kemba Walker",
+  :number => 33,
+  :shoe => 15,
+  :points => 6,
+  :rebounds => 12,
+  :assists => 12,
+  :steals => 7,
+  :blocks => 5,
+  :slam_dunks => 12)
   final_hash
 end
